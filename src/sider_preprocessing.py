5e50f65ee9e7227657e15e39ae2352acb9d94fe8
billelassn <billel-130@hotmail.fr>
2025-10-19 08:39:11 +0000

    refactor: Clean up the Preprocessing script

--- src/sider_preprocessing.py
+++ src/sider_preprocessing.py
@@ -4,18 +4,11 @@ from rdkit.Chem import PandasTools
 import numpy as np
 from typing import List
 
-def sider_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Preprocessing function for the SIDER (Classification) dataset.
-    Args:
-        df (pd.DataFrame): The raw SIDER DataFrame.
-    Returns:
-        pd.DataFrame: The cleaned DataFrame, ready for modeling.
-    """
+def sider_preprocessing(df: pd.DataFrame):
 
     df = df.copy() # Work on a copy of the dataframe
 
-    # 1. Ensure there is no NA and Null
+    # Ensure there is no NA and Null
     total_null_values = df.isnull().sum().sum()
     if total_null_values > 0:
       print(f"\nTOTAL number of null values in the DataFrame: {total_null_values}")
@@ -24,13 +17,13 @@ def sider_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     if total_null_values > 0:
       print(f"\nTOTAL number of Na in the DataFrame: {total_na_values}")
 
-    # 2. Ensure the 27 labels are numeric
+    # Ensure the 27 labels are numeric
     non_numeric_df = df.select_dtypes(exclude=np.number)
     print("\nNon-Numeric Columns (Categorical/Objects):")
     print(non_numeric_df.columns.tolist())
 
 
-    # 3. Validate and Canonicalize SMILES
+    # Validate and Canonicalize SMILES
     def canonicalize_smiles(smiles):
         '''This function takes a non-canonical SMILES and
         returns the canonical version
@@ -49,21 +42,19 @@ def sider_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     # apply canonical smiles to our df
     df['canonical_smiles'] = df['smiles'].apply(canonicalize_smiles)
 
-    # Check
     num_rows = len(df)
     num_unique_smiles = df['canonical_smiles'].nunique()
     if num_rows - num_unique_smiles != 0:
       print(f"\nTotal number of rows in the DataFrame: {num_rows}")
       print(f"\nNumber of unique canonical SMILES: {num_unique_smiles}\n")
 
-    # Replace old 'smiles' column by the canonalized smiles
+    # Replace old smiles column by the canonalized smiles
     df = df.drop(columns='smiles')
 
     last_col_name = df.columns[-1]
     col_to_move = df.pop(last_col_name)
     df.insert(0, last_col_name, col_to_move)
 
-    # Add Mol object
     PandasTools.AddMoleculeColumnToFrame(df, smilesCol='canonical_smiles', molCol='Molecule')
 
     print(f"\n--- SIDER Preprocessing Report ---\n")
