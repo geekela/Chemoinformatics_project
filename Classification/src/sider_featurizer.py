a1be457e70f0ea811a35cd371e7d2188d74c9900
sukibi <suparna.biswas@dauphine.eu>
2025-10-24 15:02:08 +0200

    featurizers to use for baselines

--- Classification/src/sider_featurizer.py
+++ Classification/src/sider_featurizer.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 from typing import Union
+import deepchem as dc
 from rdkit.ML.Descriptors.MoleculeDescriptors import MolecularDescriptorCalculator
 from rdkit.Chem import rdFingerprintGenerator, Descriptors
 
-def featurizer(df: pd.DataFrame,methods: list=None, mol_col: str = 'Molecule', fpSize: int = 2048, concatenate: bool = False) -> Union[pd.DataFrame, dict]:
+def featurizer(df: pd.DataFrame,methods: list=None, mol_col: str = 'Molecule', smiles: str= 'canonical_smiles', rdkit_all: bool =True,fpSize: int = 2048, concatenate: bool = False) -> Union[pd.DataFrame, dict]:
     """
     Calculates Morgan Fingerprints (2048 bits) and the specified RDKit descriptors
     using the MolecularDescriptorCalculator, adds them to the DataFrame, and cleans up.
@@ -12,7 +13,7 @@ def featurizer(df: pd.DataFrame,methods: list=None, mol_col: str = 'Molecule', f
 
     
     if methods is None:
-        methods = ["rdkit", "MorganFP"]
+        methods = ["rdkit", "maccs", "MorganFP"]
     # Descriptors List
     descriptors = [
         'MolWt',
@@ -26,22 +27,42 @@ def featurizer(df: pd.DataFrame,methods: list=None, mol_col: str = 'Molecule', f
 
     all_features = {}
     cols_orig = df.columns.tolist()
+    smiles=df[smiles].tolist()
     for method in methods:
         if method == "rdkit":
             df_rd=df.copy()
-            #descriptors = [desc[0] for desc in Descriptors._descList]
-            calculator = MolecularDescriptorCalculator(descriptors)
-            def calc_features(mol):
-                if mol is None:
-                    return [np.nan] * len(descriptors)
-                return list(calculator.CalcDescriptors(mol))
-            properties_series = df[mol_col].apply(calc_features)
-            df_rdkit_features = pd.DataFrame(properties_series.tolist(), columns=descriptors, index=df.index)
+            if rdkit_all==False:
+                calculator = MolecularDescriptorCalculator(descriptors)
+                def calc_features(mol):
+                    if mol is None:
+                        return [np.nan] * len(descriptors)
+                    return list(calculator.CalcDescriptors(mol))
+                properties_series = df[mol_col].apply(calc_features)
+                df_rdkit_features = pd.DataFrame(properties_series.tolist(), columns=descriptors, index=df.index)
+            else:
+                featurizer = dc.feat.RDKitDescriptors()
+                rdkit_feat = featurizer.featurize(smiles)
+                feature_names = [f"RDKit_{i}" for i in range(rdkit_feat.shape[1])]
+                df_rdkit_features = pd.DataFrame(rdkit_feat, columns=feature_names)
+                     
             df_feat=pd.concat([df_rd, df_rdkit_features], axis=1)
             nan_col_list=df_feat.columns[df_feat.isna().any()].tolist()
             df_feat=df_feat.drop(columns=nan_col_list)
             all_features[method] = df_feat
             print(f"df with RDKit features shape: {df_feat.shape}")
+            
+        elif method == "maccs":
+            df_mc=df.copy()
+            featurizer = dc.feat.MACCSKeysFingerprint()
+            maccs_feat = featurizer.featurize(smiles)
+            feature_names = [f"MACCS_{i}" for i in range(maccs_feat.shape[1])]
+            df_maccs_features = pd.DataFrame(maccs_feat, columns=feature_names)
+            df_feat=pd.concat([df, df_maccs_features], axis=1)
+            nan_col_list=df_feat.columns[df_feat.isna().any()].tolist()
+            df_feat=df_feat.drop(columns=nan_col_list)
+            all_features[method] = df_feat
+            print(f"df with MACCS features shape: {df_feat.shape}")
+            
         elif method == "MorganFP":
             # MORGAN FINGERPRINTS
             df_mf=df.copy()
@@ -104,7 +125,7 @@ def featurizer(df: pd.DataFrame,methods: list=None, mol_col: str = 'Molecule', f
         for feat_df in all_features.values():
             df_concat = pd.concat([df_concat, feat_df.drop(columns=cols_orig,errors='ignore')],axis=1)
         print(f"Feature engineering complete. Final DataFrame shape: {df_concat.shape}")
-        print(f"Total features added: {df_rdkit_features.shape[1]} rdkit + {df_mf_features.shape[1]} Morgan")
+        print(f"Total features added: {df_rdkit_features.shape[1]} rdkit+ {df_maccs_features.shape[1]} maccs + {df_mf_features.shape[1]} Morgan")
         return df_concat
     
     else:
