300dfa9971252cbf4f556a6bcc191101b3f26416
sukibi <suparna.biswas@dauphine.eu>
2025-10-24 15:08:50 +0200

    svm optimization updated

--- Classification/src/sider_svm_optimization.py
+++ Classification/src/sider_svm_optimization.py
@@ -2,8 +2,9 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 from scipy.stats import loguniform
+from src.sider_baseline_models import evaluate_multilabel_model_internal
 
-def optimize_svm(X_train_scaled, y_train):
+def optimize_svm(X_train_scaled, y_train, X_test_scaled, y_test):
     """
     Optimizes a multi-label SVM model using RandomizedSearchCV on pre-scaled training data.
     """
@@ -13,7 +14,7 @@ def optimize_svm(X_train_scaled, y_train):
     )
 
     param_distributions = {
-        'estimator__kernel': ['rbf'],
+        'estimator__kernel': ['rbf', 'linear'],
         'estimator__C': [0.1, 1, 10, 50, 100],
         'estimator__gamma': ['scale', 0.001, 0.01, 0.1]
     }
@@ -22,7 +23,7 @@ def optimize_svm(X_train_scaled, y_train):
         estimator=svm_base,
         param_distributions=param_distributions,
         n_iter=8,
-        cv=3,
+        cv=5,
         scoring='roc_auc_ovr',
         n_jobs=-1,
         random_state=42,
@@ -34,5 +35,9 @@ def optimize_svm(X_train_scaled, y_train):
     random_search_svm.fit(X_train_scaled, y_train)
 
     print(f"\nSVM - Best Hyperparameters found: {random_search_svm.best_params_}")
+    best_svm=random_search_svm.best_estimator_
+    macro_auc, detailed_df = evaluate_multilabel_model_internal(best_svm, X_test_scaled, y_test)
+    print(f"\nSVM Test Macro AUC-ROC: {macro_auc:.4f}")
 
-    return random_search_svm.best_estimator_
+
+    return best_svm, macro_auc, detailed_df
